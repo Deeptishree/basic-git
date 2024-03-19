9bba296171e51abb0eabaec6e58bf1cf829c43e8
Deepti <Deeptisri087@gmail.com>
2024-03-19 17:46:46 +0530

    modified java & text files

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
 class Demo{
     public static void main(String [] args);
     System.out.println("hello");
+    System.out.println("DevOops learning");
+    System.out.println("enjoying");
     
 }
--- readme.txt
+++ readme.txt
@@ -1,2 +1,5 @@
 hello!!!!
 learning basic DevOops!!!
+its very interesting!!!!
+i am enjoying the DevOops session!!!
+
